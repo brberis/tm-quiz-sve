69b302c380c3eda2e23ae6cc9243a64b78409254
Cristobal Barberis <cristobal@barberis.com>
2022-07-01 21:13:09 -0400

    index comments

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,28 @@
+var time = 150;
+var countdown = null;
+var pageContentEl = document.querySelector("#content");
+
+
+function setTimer() {
+  document.getElementById("tm").textContent = time;
+  if (time == 0) {
+    stopCountDown();
+  }
+  time--;
+}
+
+function stopCountDown() {
+    clearInterval(countdown);
+}
+
+var taskButtonHandler = function (event) {
+  // get target element from event
+  var targetEl = event.target;
+
+  if (targetEl.matches("#start")) {
+    countdown = setInterval(setTimer, 1000);
+  }
+};
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
