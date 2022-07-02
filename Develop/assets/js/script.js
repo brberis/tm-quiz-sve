76c98b7a9869fbe4ace965e6868b640b3526428b
Cristobal Barberis <cristobal@barberis.com>
2022-07-02 01:29:13 -0400

    question constructor

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,11 +1,12 @@
 var time = 150;
 var countdown = null;
-var pageContentEl = document.querySelector("#content");
-
+var homeContentEl = document.querySelector("#home");
+var quizContentEl = document.querySelector("#question");
+console.log(quiz);
 
 function setTimer() {
   document.getElementById("tm").textContent = time;
-  if (time == 0) {
+  if (time < 1) {
     stopCountDown();
   }
   time--;
@@ -18,11 +19,64 @@ function stopCountDown() {
 var taskButtonHandler = function (event) {
   // get target element from event
   var targetEl = event.target;
-  console.log('start');
   if (targetEl.matches("#start")) {
     countdown = setInterval(setTimer, 1000);
+    startQuestions();
   }
 };
 
+var startQuestions = function() {
+  homeContentEl.style.display = 'none';
+  constructQuestion(0);
+}
 
-pageContentEl.addEventListener("click", taskButtonHandler);
+var constructQuestion = function (index) {
+  // questionContentEl.
+  var questionInfo = document.createElement("div");
+  questionInfo.className = "quiz";
+  questionInfo.innerHTML = "<h2>" + quiz[index].question;
+  quizContentEl.appendChild(questionInfo);
+
+  var questionOptionsEl = document.createElement("div");
+  questionOptionsEl.className = "options";
+  quizContentEl.appendChild(questionOptionsEl);
+
+
+  for (var i = 0; i < quiz[index].options.length; i++) {
+    var option = quiz[index].options[i];
+    var optionEl = document.createElement("buttom");
+    optionEl.setAttribute("data-question-id", index);
+    optionEl.setAttribute("data-answer", option);
+    optionEl.className = "btn question";
+    optionEl.textContent = (i+1) + ". " + option;
+    questionOptionsEl.appendChild(optionEl);
+  }
+
+
+  quizContentEl.addEventListener("click", optionsButtonHandler);
+
+
+}
+
+var optionsButtonHandler = function(event) {
+  var questionId = event.target.getAttribute('data-question-id');
+  var userAnswer = event.target.getAttribute('data-answer');
+
+  if (userAnswer == quiz[questionId].answer) {
+    console.log("Correcto");
+  }else{
+    time = time - 15;
+  }
+
+
+}
+
+
+
+// checkAnswer = function(question) {
+//   if (question.answer == ) {
+    
+//   }
+// }
+
+homeContentEl.addEventListener("click", taskButtonHandler);
---
+++ Develop/quiz.js
@@ -0,0 +1,54 @@
+
+    var quiz = [
+      {
+        "question": "String values must be enclosed within ________ when being assigned to variables.",
+        "options": [
+          "commas",
+          "curly brackets",
+          "quotes",
+          "parenthesis"
+          ],
+        "answer": "quotes"
+      },
+      {
+        "question": "A very useful tool used during developmentand debugging for printing content to the debugger is:",
+        "options": [
+          "JavaScript",
+          "cterminal/bash",
+          "for loops",
+          "console.log"
+          ],
+        "answer": "console.log"
+      },
+      {
+        "question": "The condition in an if / else statement is enclosed with _____ .",
+        "options": [
+          "quotes",
+          "curly brackets",
+          "parenthesis",
+          "square brackets"
+          ],
+        "answer": "parenthesis"
+      },
+      {
+        "question": "Arrays in JavaScript can be used to store ______ .",
+        "options": [
+          "numbers and strings",
+          "other arrays",
+          "booleans",
+          "all of the above"
+          ],
+        "answer": "all of the above"
+      },
+      {
+        "question": "Commonly used data types DO Not Include:",
+        "options": [
+          "strings",
+          "booleans",
+          "alerts",
+          "numbers"
+          ],
+        "answer": "alerts"
+      }
+
+    ];
