6486aa119962a260ea29d692aaa4c031818c2dde
Cristobal Barberis <cristobal@barberis.com>
2022-07-02 03:06:33 -0400

    feature/questions

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,7 @@
 var time = 150;
+var questionNum = 0;
 var countdown = null;
+var scores = [];
 var homeContentEl = document.querySelector("#home");
 var quizContentEl = document.querySelector("#question");
 console.log(quiz);
@@ -7,16 +9,19 @@ console.log(quiz);
 function setTimer() {
   document.getElementById("tm").textContent = time;
   if (time < 1) {
-    stopCountDown();
+    endQuiz();
   }
   time--;
 }
 
-function stopCountDown() {
+function endQuiz() {
     clearInterval(countdown);
+    
+    window.location.href = "/scores.htmi";
 }
 
-var taskButtonHandler = function (event) {
+var startButtonHandler = function (event) {
+  questionNum = 0;
   // get target element from event
   var targetEl = event.target;
   if (targetEl.matches("#start")) {
@@ -27,15 +32,22 @@ var taskButtonHandler = function (event) {
 
 var startQuestions = function() {
   homeContentEl.style.display = 'none';
-  constructQuestion(0);
-}
+  
+  if (time > 0 && questionNum + 1 <= quiz.length) {
+    constructQuestion(questionNum);
+  }else{
+    endQuiz();
+  }
+
+} 
+
 
 var constructQuestion = function (index) {
   // questionContentEl.
-  var questionInfo = document.createElement("div");
-  questionInfo.className = "quiz";
-  questionInfo.innerHTML = "<h2>" + quiz[index].question;
-  quizContentEl.appendChild(questionInfo);
+  var questionInfoEl = document.createElement("div");
+  questionInfoEl.className = "quiz";
+  questionInfoEl.innerHTML = "<h2>" + quiz[index].question;
+  quizContentEl.appendChild(questionInfoEl);
 
   var questionOptionsEl = document.createElement("div");
   questionOptionsEl.className = "options";
@@ -47,36 +59,33 @@ var constructQuestion = function (index) {
     var optionEl = document.createElement("buttom");
     optionEl.setAttribute("data-question-id", index);
     optionEl.setAttribute("data-answer", option);
-    optionEl.className = "btn question";
+    optionEl.className = "btn opt";
     optionEl.textContent = (i+1) + ". " + option;
     questionOptionsEl.appendChild(optionEl);
   }
 
 
-  quizContentEl.addEventListener("click", optionsButtonHandler);
+  quizContentEl.addEventListener("click", optionsButtonHandler);  
 
 
 }
 
 var optionsButtonHandler = function(event) {
-  var questionId = event.target.getAttribute('data-question-id');
+  // var questionId = event.target.getAttribute('data-question-id');
   var userAnswer = event.target.getAttribute('data-answer');
-
-  if (userAnswer == quiz[questionId].answer) {
+  if (userAnswer == quiz[questionNum].answer) {
     console.log("Correcto");
   }else{
     time = time - 15;
   }
-
+  questionNum++
+  var questionInfoEl = document.querySelector(".quiz");
+  var questionOptionsEl = document.querySelector(".options");
+  quizContentEl.removeChild(questionInfoEl);
+  quizContentEl.removeChild(questionOptionsEl);
+  startQuestions();
 
 }
 
 
-
-// checkAnswer = function(question) {
-//   if (question.answer == ) {
-    
-//   }
-// }
-
-homeContentEl.addEventListener("click", taskButtonHandler);
+homeContentEl.addEventListener("click", startButtonHandler);
